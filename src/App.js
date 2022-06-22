3f85018af8ca655303b1ede2c14b9921fb4b5154
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-06-22 18:41:48 +0100

    add background e icone ao input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import { FiSearch } from 'react-icons/fi';
 function App() {
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
-     
+      
      <div className="containerInput">
-      <input type="text" placeholder="Digite seu CEP"></input>
+      <input type="text" placeholder="Digite seu CEP" maxlength="9"></input>
+      <button   type="button" className="botao"><FiSearch size={25} color="#000000"/></button>
      </div>
 
     </div>
