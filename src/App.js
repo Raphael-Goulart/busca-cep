32b18e669afcd96f8077c41179ba1be5e1fc35e7
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-06-25 12:09:26 +0100

    adicionado texto de teste

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
       <h1 className="title">Buscador CEP</h1>
       
      <div className="containerInput">
-      <input type="text" placeholder="Digite seu CEP" maxlength="9"></input>
+      <input type="text" placeholder="Digite seu CEP"></input>
       <button   type="button" className="botao"><FiSearch size={25} color="#000000"/></button>
      </div>
 
@@ -13,7 +13,9 @@ function App() {
       <h2>CEP: 05848-000</h2>
 
       <span>Rua Teste</span>
-      <span>Complemento, Bairro, Cidade Teste</span>
+      <span>Bairro</span>
+      <span>Complemento</span>
+      <span>Cidade / Estado</span>
     </main>
     </div>
   );
